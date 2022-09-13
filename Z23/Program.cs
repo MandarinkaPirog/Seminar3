c465698d7474fa6deeefd1a4a83f03cbf6f0f435
Vika <mandarinkavikabakalova@gmail.com>
2022-09-13 20:25:06 +0400

    Задача 23

---
+++ Z23/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов
+// чисел от 1 до N.
+
+Console.Write("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+Cube(num);
+void Cube(int num)
+{
+  int index=1;
+  while (index<=num)  
+  {
+    Console.Write(+Math.Pow(index,3) +",");
+    index++;
+  }
+}
